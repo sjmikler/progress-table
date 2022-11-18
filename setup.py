b8cf55a0f030facd49612de4a847f2c279a0692c
Szymon Mikler <sjmikler@gmail.com>
2022-11-18 21:28:22 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="progress-table",
-    version="0.0.16",
+    version="0.0.17",
     url="https://github.com/gahaalt/progress-table.git",
     author="Szymon Mikler",
     author_email="sjmikler@gmail.com",
