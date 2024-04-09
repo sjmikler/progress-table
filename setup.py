f49b06ab509c861f47a164c2547a94c9bf2c509e
Szymon Mikler <sjmikler@gmail.com>
2024-04-10 00:30:52 +0200

    Dropping python=3.7

--- setup.py
+++ setup.py
@@ -30,11 +30,10 @@ setup(
     license="MIT",
     description="Display progress as a pretty table in the command line.",
     packages=find_packages(),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
