bc35bfde7d05454df0d841525f79cfff74532dc0
Szymon Mikler <sjmikler@gmail.com>
2024-04-29 19:39:34 +0200

    Fix imports

--- progress_table/__init__.py
+++ progress_table/__init__.py
@@ -3,7 +3,7 @@
 __version__ = "2.2.0"
 
 from progress_table.v0.progress_table import ProgressTableV0
-from progress_table.v1 import styles
+from progress_table.v1 import styles, common
 from progress_table.v1.progress_table import ProgressTableV1
 
 ProgressTable = ProgressTableV1
