44257a8205db574d314c5e1b6cb8e4be6f32ce88
Szymon Mikler <sjmikler@gmail.com>
2025-03-09 19:58:53 +0100

    Change error messages

--- examples/training.py
+++ examples/training.py
@@ -6,14 +6,14 @@ import time
 try:
     import numpy as np
 except ImportError:
-    raise ImportError("Numpy is required to run this example. Run: pip install numpy.")
+    raise ImportError("Numpy is required to run the example!")
 
 try:
     from sklearn.datasets import load_iris
     from sklearn.model_selection import train_test_split
     from sklearn.utils import shuffle
 except ImportError:
-    raise ImportError("Scikit-learn is required to run this example. Run: pip install scikit-learn.")
+    raise ImportError("Scikit-learn is required to run the example!")
 
 from progress_table import ProgressTable
 
