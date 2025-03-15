6e408f65627003a674b9acbfaa095d459f28b95b
Szymon Mikler <sjmikler@gmail.com>
2025-03-15 01:43:27 +0100

    Fixing README on PyPi

--- hooks.py
+++ hooks.py
@@ -9,10 +9,9 @@ is useful for PyPI. Otherwise the README.md
 file would be rendered without the images.
 """
 
-import tempfile
 from pathlib import Path
-
-from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+from hatchling.plugin import hookimpl
+from hatchling.metadata.plugin.interface import MetadataHookInterface
 
 
 def with_direct_github_urls(text):
@@ -23,17 +22,13 @@ def with_direct_github_urls(text):
     return text.replace("(docs", "(" + docs_github_link)
 
 
-class CustomBuildHook(BuildHookInterface):
-    def initialize(self, version, build_data) -> None:
+class CustomBuildHook(MetadataHookInterface):
+    def update(self, metadata: dict):
         readme_path = Path("README.md")
         original_text = readme_path.read_text(encoding="utf-8")
         updated_text = with_direct_github_urls(original_text)
         with open("README_pypi.md", "w", encoding="utf-8") as f:
             f.write(updated_text)
-        # Tell Hatch to use this modified README
-        build_data["readme"] = f.name
 
-    def finalize(self, version, build_data, artifact_path) -> None:
-        p = Path(build_data["readme"])
-        if p.exists():
-            p.unlink()
+        # Tell Hatch to use this modified README
+        metadata["readme"] = f.name
